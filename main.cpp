9482cb40bd4a5bb4dc0ed428d143499bb2d7440f
Lillian MaGill <lmagill9788@floridapoly.edu>
2026-04-14 19:01:31 +0000

    Project 2

--- main.cpp
+++ main.cpp
@@ -1,41 +1,75 @@
-#include "Graph.h"
-#include "Vertex.h"
-#include "Graph.cpp"
-
-#include <string>
-#include <fstream>
 #include <vector>
+#include <fstream>
+#include <string>
 #include <iostream>
+using namespace std;
+
+vector <vector<char>> CSVLine(const char* line){
+
+    vector<vector<char>> fields;
+    vector<char> current;
+    bool hasQuotes = false;
+
+    
+    for(int i = 0; line[i] != '\0'; i++){
+
+        char c = line[i];
+
+        if(c == '"'){
+            hasQuotes = !hasQuotes;
+            
+        }else if(c == ','){
+
+            current.push_back('\0');
+            fields.push_back(current);
+            current.clear();
+
+        } else{
+            current.push_back(c);
+        }
+    }
 
 
+    current.push_back('\0');
+    fields.push_back(current);
+
+    return fields;
+
+}
 
 int main(){
 
-    ifstream file("airplanes.csv");
+   const char* filename = "airports.csv";
+   ifstream file(filename);
 
-    if(!file.is_open()){
+   if(!file.is_open()){
 
-        cout << "File could not open" << endl;
-        return 1;
-    }
+    cout << "File could not open" << endl;
+    return 1;
 
-    string line;
+   }
 
-    while(getline(file, line)){
+   char line[1024];
+   while(file.getline(line, sizeof(line))){
 
-        vector<string> fields = Quotes(line);
+        vector<vector<char>> fields = CSVLine(line);
 
-        for(int i = 0; i < fields.size(); i++){
+        for(int i = 0; i < fields.size(); ++i){
 
-            cout << field[i] << " " << endl;
+            cout << "Field"<< i + 1 << ": ";
+
+            for(int j = 0; fields[i][j] != '\0'; ++j){
+                cout << fields[i][j] ;
+                
+            }
+
+            cout  << endl;
 
         }
 
-    }
+   }
 
-    file.close();
-
-
+   file.close();
     return 0;
 
 }
