fca03d5d9143308d5572c73faa97962e6d55a7a7
Lillian MaGill <lmagill9788@floridapoly.edu>
2026-04-21 16:26:53 +0000

    Graph

--- Graph.h
+++ Graph.h
@@ -1,18 +1,26 @@
 #ifndef GRAPH_H
 #define GRAPH_H
+
 #include "Vertex.h"
 #include "Edge.h"
 #include <vector>
+#include <string>
+#include <iostream>
 
 
 template <typename T>
 class Graph {
 public:
+    //Create a graph
     Graph() {}
 
+    //Add an airport
     void insert_vertex(const Vertex<T>& ver);
+
+    //Add an edge
     void add_edge(const Vertex<T>& ver1, const Vertex<T>& ver2, int weight); //connect ver1 with ver2
 
+    //Prints map of airports
     void print() const;
 
     void DFS(Vertex<T>& ver);
@@ -20,7 +28,7 @@ public:
     int dijkstra_shortest_path(const Vertex<T>& src, const Vertex<T>& dest);
 
 private:
-    std::vector<Vertex<T>> vertices; //nodes
+    std::vector<Vertex<T>> vertices; //list of airports 
     std::vector<std::vector<Edge>> edges; //connections
 
     void clean_visited();
